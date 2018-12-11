f301fe85d9901ac478faf519f57fde6cbc61eb75
d-mich <40661164+d-mich@users.noreply.github.com>
2018-12-11 16:48:35 +0100

    update

--- Trash-it/src/pages/tabs/tabs.ts
+++ Trash-it/src/pages/tabs/tabs.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
+//pages
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
+  //impostazione dei tabs
   tab1Root = HomePage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab3Root = SettingsPage;
 
   constructor() {
 
