5caa090dcad0004a51cc5e1c08e3c9e2d306dc54
d-mich <40661164+d-mich@users.noreply.github.com>
2018-12-11 16:33:55 +0100

    Update app.module.ts

--- Trash-it/src/app/app.module.ts
+++ Trash-it/src/app/app.module.ts
@@ -1,11 +1,17 @@
+//APP.MODULES.TS FILE PRICIPALE IN CUI DEVE ESSERE DICHIARATO TUTTO
+
+//default
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+//pages import {nome scelto} from 'path nel progetto'
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { SettingsPage } from '../pages/settings/settings';
+import { ContactPage } from '../pages/contact/contact';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    SettingsPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    SettingsPage,
     TabsPage
   ],
   providers: [
