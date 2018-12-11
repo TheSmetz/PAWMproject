29f31bff84bde7b24de7a3acaedd5204fd8ed970
d-mich <40661164+d-mich@users.noreply.github.com>
2018-12-11 20:03:02 +0100

    update

--- Trash-it/src/pages/home/home.ts
+++ Trash-it/src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+   //città (da prendere in input successivamente)
+   location:{
+    citta:string,
+    provincia:string
+   }
+ 
+  constructor(public navCtrl: NavController, private storage:Storage) {
+  }
 
+  ionViewWillEnter(){
+    this.storage.get('location').then((val) => {
+      if (val != null){
+        this.location.citta = 'Ciao';
+      } else {
+        this.location = {
+          citta: 'Camerino',
+          provincia: 'MC'
+        }
+      }
+    })
   }
 
 }
--- Trash-it/src/pages/settings/settings.ts
+++ Trash-it/src/pages/settings/settings.ts
@@ -15,8 +15,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
+  
+    //citta:string;
+    //provincia:string;
+   
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+   //this.citta = "Camerino";
+   //this.provincia = "MC";
+
+   }
+
+   //getLocation() {
+    //if (this.citta == "" || this.provincia == "") {
+   //   alert ("Inserisci città");
+   // }else alert ("Città = "+ this.citta + "Provincia = "+ this.provincia);
+
+  //}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
