718ff033d3a4a2de9388b6da7b9ed35f90680b3e
d-mich <40661164+d-mich@users.noreply.github.com>
2018-12-12 11:24:31 +0100

    update

--- Trash-it/src/pages/home/home.ts
+++ Trash-it/src/pages/home/home.ts
@@ -5,15 +5,21 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-   //città (da prendere in input successivamente)
-   location:{
-    citta:string,
-    provincia:string
-   }
+  codiceABarre:string;
  
   constructor(public navCtrl: NavController) {
+    this.codiceABarre = "";
+  }
+
+  getBarcode(){
+    if(this.codiceABarre == ""){
+      alert("Inserire codice a barre valido");
+    } else {
+      alert("Codice inserito " + this.codiceABarre);
+    }
   }
 
   ionViewWillEnter(){
